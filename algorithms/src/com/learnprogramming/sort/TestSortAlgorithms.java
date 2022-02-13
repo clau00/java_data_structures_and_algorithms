7092de880fb76764092c0c9bc360f7a8bed7d0f3
claudiu <danbalint_88@yahoo.com>
2022-02-13 13:05:52 +0200

    CB: Algorithms - Sort: refactor code

--- algorithms/src/com/learnprogramming/sort/TestSortAlgorithms.java
+++ algorithms/src/com/learnprogramming/sort/TestSortAlgorithms.java
@@ -1,6 +1,9 @@
 package com.learnprogramming.sort;
 
+import java.util.Arrays;
+
 import com.learnprogramming.sort.bubble.BubbleSort;
+import com.learnprogramming.sort.selection.SelectionSort;
 
 public class TestSortAlgorithms {
 
@@ -8,13 +11,29 @@ public class TestSortAlgorithms {
 
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
-        //  apply bubble sort
-        BubbleSort.sort(intArray);
-        printArray(intArray);
-
+		applySort(Arrays.copyOf(intArray, intArray.length), "Bubble Sort");
+		applySort(Arrays.copyOf(intArray, intArray.length), "Selection Sort");
     }
 
-    public static void printArray(int[] array) {
+    private static void applySort(int[] array, String sortType) {
+    	switch(sortType) {
+			case "Bubble Sort":
+				printArray(null, array);
+				BubbleSort.sort(array);
+				printArray(sortType, array);
+				break;
+			case "Selection Sort":
+				printArray(null, array);
+				SelectionSort.sort(array);
+				printArray(sortType, array);
+				break;
+			default:
+				System.out.println("Please enter \"Bubble Sort\" or \"Selection Sort\"!");
+		}
+	}
+
+    private static void printArray(String typeOfSort, int[] array) {
+		System.out.println("\n\t" + (typeOfSort == null ? "Unsorted Array:" : typeOfSort + ":"));
         for(int i = 0; i < array.length; i++) {
             if(i == array.length - 1) {
                 System.out.print(array[i] + "\n");
@@ -22,6 +41,5 @@ public class TestSortAlgorithms {
                 System.out.print(array[i] + ", ");
             }
         }
-        System.out.println(" ===================== ");
     }
 }
